7fed77da79a1ee85405bd90ce3686b4af35f6c25
Oleksii Drenov <leshadrenev@gmail.com>
2025-04-03 00:21:36 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,15 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cashe_dict = {}
+
+    def wrapper(*args, **kwargs) -> Callable:
+        for i in args:
+            if args not in cashe_dict:
+                cashe_dict[args] = func(*args)
+                print("Calculating new result")
+                return cashe_dict[args]
+            else:
+                print("Getting from cache")
+                return cashe_dict[args]
+    return wrapper
